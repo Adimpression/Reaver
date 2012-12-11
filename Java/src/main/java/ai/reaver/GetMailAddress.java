fc2bd437debff57b511fbcb0d94d0f761ea8c87a
System Administrator <root@Ravindranath-Akilas-MacBook-Air.local>
2012-12-11 22:33:09 +0800

    This is not the initial commit... Kidding! :D

---
+++ Java/src/main/java/ai/reaver/GetMailAddress.java
@@ -0,0 +1,17 @@
+package ai.reaver;
+
+import ai.scribble.License;import java.lang.String;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ravindranath Akila
+ * Date: Nov 10, 2010
+ * Time: 8:38:36 PM
+ */
+@License(content = "This code is licensed under GNU AFFERO GENERAL PUBLIC LICENSE Version 3")
+public interface GetMailAddress {
+    /**
+     * @return Email Address
+     */
+    public String email();
+}
